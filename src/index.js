9b37cf4e8602a13a33c5a86e14af52b947fc993f
robert-virtual <robertocastill63@gmail.com>
2022-06-20 19:21:05 -0600

    express session

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const session = require("express-session")
+
 const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 4000
@@ -7,9 +9,21 @@ global.__prod__ = process.env.NODE_ENV == "production"
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(session({
+  name: "qid",
+  saveUninitialized: false,
+  secret: "hola jeje",
+  resave: false,
+  cookie: {
+    //secure: global.__prod__,
+    maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // dias years
+    //httpOnly: true,
+    sameSite: "lax"
+  }
+}))
 
 //routes
-app.use("/",require("./routes/auth"))
+app.use("/", require("./routes/auth"))
 
 app.listen(port, () => {
   console.log(`server (${global.__prod__ ? 'prod' : 'dev'}) running on port ${port}...`)
